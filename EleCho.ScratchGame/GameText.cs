df85013de7f4e7b3f33b5529fd8fa26816c13bf4
SlimeNull <slimenull@outlook.com>
2022-08-18 20:00:23 +0800

    添加了 GameText (游戏文本

---
+++ EleCho.ScratchGame/GameText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EleCho.ScratchGame
+{
+    public class GameText : GameObject
+    {
+        public override void Render()
+        {
+            
+        }
+    }
+}
